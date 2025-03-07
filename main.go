cc16e71be67229d14ad06e38a2bf148f1c9594c8
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-03-06 18:21:58 -0800

    feat: get it working somewhat

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/w-h-a/pkg/telemetry/log"
 	memorylog "github.com/w-h-a/pkg/telemetry/log/memory"
 	"github.com/w-h-a/pkg/utils/memoryutils"
+	"github.com/w-h-a/proxy/src"
 	"github.com/w-h-a/proxy/src/config"
+	"github.com/w-h-a/proxy/src/services/fault"
+	httpdelay "github.com/w-h-a/proxy/src/services/fault/httpDelay"
 )
 
 func main() {
@@ -25,4 +30,48 @@ func main() {
 	)
 
 	log.SetLogger(logger)
+
+	// traces
+
+	// other dependencies
+	manager := fault.NewManager()
+
+	manager.Register(func(options fault.FaultOptions) (fault.Fault, error) {
+		return httpdelay.NewFault(options), nil
+	}, "httpdelay")
+
+	// reverse proxies
+	httpServer := src.AppFactory(manager)
+
+	// wait group and error chan
+	wg := &sync.WaitGroup{}
+	errCh := make(chan error, 1)
+
+	// start http server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		errCh <- httpServer.Start()
+	}()
+
+	// block
+	err := <-errCh
+	if err != nil {
+		log.Errorf("failed to start server: %+v", err)
+	}
+
+	// graceful shutdown
+	wait := make(chan struct{})
+
+	go func() {
+		defer close(wait)
+		wg.Wait()
+	}()
+
+	select {
+	case <-wait:
+	case <-time.After(30 * time.Second):
+	}
+
+	log.Info("successfully stopped server")
 }
